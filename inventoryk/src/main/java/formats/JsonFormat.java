8039d239f7d063ba7391fe87d2fbaa1f3dd3c2a7
Dimitrios Kalerantes <Dimitrios.Kalerantes@arhs-dev-hellas.com>
2022-07-11 13:01:08 +0300

    create Tests and the JsonFormat class

---
+++ inventoryk/src/main/java/formats/JsonFormat.java
@@ -0,0 +1,63 @@
+package formats;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import intentory.kalerantes.Item;
+import intentory.kalerantes.ItemList;
+
+public class JsonFormat {
+	
+	
+	private ItemList items;
+	
+	public JsonFormat(ItemList items) {
+		this.items=items;
+		
+	}
+	
+	public ItemList getItems() {
+		return items;
+	}
+
+
+	
+//	public String convertToJson() {
+//		Gson json=new Gson();
+//	String strJson=json.toJson(items.getLi());
+//	
+//		
+//		
+//		return strJson;
+//	}
+	public String convertToJson() {
+		Gson gson=new GsonBuilder().setPrettyPrinting().create();
+		String strJson=gson.toJson(items.getLi());
+		
+		return strJson;
+		
+		
+	}
+	
+	
+
+	public void createFileWithJsonFormat()  {
+		File file=new File("text.json");
+		try(FileWriter fWriter=new FileWriter(file)){
+			fWriter.write(convertToJson());
+			
+		}catch(IOException e) {
+			System.out.println(e);
+		}
+		
+		
+	}
+	
+	
+	
+
+}
---
+++ inventoryk/src/test/groovy/formats/JsonFormatSpec.groovy
@@ -0,0 +1,91 @@
+package formats
+
+import intentory.kalerantes.Item
+import intentory.kalerantes.ItemList
+import spock.lang.Specification
+
+
+class JsonFormatSpec extends Specification {
+	
+	def "initalise JsonFormat"(){
+	
+	when:
+	ItemList items=new ItemList()
+	JsonFormat jFormat=new JsonFormat(items)
+	
+	then:
+	jFormat.items==items
+	
+	}
+	
+	def "convert ItemList to Json Format"(){
+		given:
+		Item item=new Item("SKODA","XSWER234R",2000)
+		
+		ItemList items=new ItemList()
+		items.getLi().add(item)
+		JsonFormat jFormat=new JsonFormat(items)
+		
+		when:
+		String result=jFormat.convertToJson()
+		
+		then:
+		System.out.println(result);
+		result==("[\n"
+			+ "  {\n    \"name\": \"SKODA\",\n"
+			+"    \"serialNumber\": \"XSWER234R\",\n"
+			+"    \"value\": 2000\n"
+			+"  }\n"
+			+"]")
+				
+		
+	}
+	
+	def "loop convert ItemList to Json Format"(){
+		given:
+		Item item=new Item("SKODA","XSWER234R",2000)
+		Item item1=new Item("BMW","WEDFG12WQ",10000)
+		
+		ItemList items=new ItemList()
+		items.getLi().add(item)
+		items.getLi().add(item1)
+		JsonFormat jFormat=new JsonFormat(items)
+		
+		when:
+		String result=jFormat.convertToJson()
+		
+		then:
+		result==("[\n"
+			+ "  {\n    \"name\": \"SKODA\",\n"
+			+"    \"serialNumber\": \"XSWER234R\",\n"
+			+"    \"value\": 2000\n"
+			+"  },\n"
+			+ "  {\n    \"name\": \"BMW\",\n"
+			+"    \"serialNumber\": \"WEDFG12WQ\",\n"
+			+"    \"value\": 10000\n"
+			+"  }\n"
+			+"]")
+		
+	}
+	
+	def "Save JsonFormat to File"(){
+		given:
+		Item item=new Item("SKODA","XSWER234R",2000)
+		
+		ItemList items=new ItemList()
+		items.getLi().add(item)
+		JsonFormat jFormat=new JsonFormat(items)
+		File file=new File("text.json")
+		FileWriter fWriter=new FileWriter(file)		
+		
+		when:
+		jFormat.createFileWithJsonFormat()
+		
+		then:
+		fWriter.write(jFormat.convertToJson())
+		
+		
+	}
+
+
+}
