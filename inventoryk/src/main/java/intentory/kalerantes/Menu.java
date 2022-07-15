201197ec6a28efb44658b79b86a6b122d0353d24
Stelios Omirou <omirous@interamerican.gr>
2022-07-15 15:47:07 +0300

    introducing the menu

---
+++ inventoryk/src/main/java/intentory/kalerantes/Menu.java
@@ -0,0 +1,16 @@
+package intentory.kalerantes;
+
+public class Menu {
+
+	public String options() {
+		return "WELCOME TO TRACKING INVETORY - PROJECT\n" +
+		"**************************************\n" +
+		"1.Add Item\n" +
+		"2.Print Out HTML format\n" +
+		"3.Save Inventory To Html file\n" +
+		"4.Print Out Csv format\n" +
+		"5.Save Inventory to Json file\n" +
+		"6.Exit The program\n";
+	}
+
+}
---
+++ inventoryk/src/test/groovy/intentory/kalerantes/MenuSpec.groovy
@@ -0,0 +1,25 @@
+package intentory.kalerantes
+
+
+import spock.lang.Specification
+
+class MenuSpec extends Specification {
+
+	def "menu options"() {
+		given:
+		Menu menu = new Menu();
+
+		expect:
+		menu.options() == """WELCOME TO TRACKING INVETORY - PROJECT
+**************************************
+1.Add Item
+2.Print Out HTML format
+3.Save Inventory To Html file
+4.Print Out Csv format
+5.Save Inventory to Json file
+6.Exit The program
+"""
+	}
+
+
+}
