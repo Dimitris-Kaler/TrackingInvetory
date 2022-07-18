091b52129bc74f1963eaeef1734734222095b4a2
Stelios Omirou <omirous@interamerican.gr>
2022-07-18 23:27:48 +0300

    test for ItemCLIParser class

---
+++ inventoryk/src/test/groovy/inventory/kalerantes/ItemCLIParserSpec.groovy
@@ -0,0 +1,39 @@
+package inventory.kalerantes
+
+import spock.lang.Specification
+
+class ItemCLIParserSpec extends Specification {
+
+	def "capture user input, create an item and put it on the list"() {
+		//TODO a big part of this test is duplicated but I think that add item method is going to be deleted
+		//so I will handle it later
+		given: "item properties"
+		String name = "name"
+		String sn = "123456789"
+		String value = "20"
+
+		and: 'a scanner that simulates the user input'
+		Scanner scanner = new Scanner(simulateUserInput(name, sn, value))
+
+		and: 'and print stream for capturing program output'
+		OutputStream captureOutput = new ByteArrayOutputStream()
+		PrintStream out = new PrintStream(captureOutput)
+
+		when: 'parsing an item'
+		Item item = new ItemCLIParser().parseItem(scanner, out)
+
+
+		then: 'this item goes inside the list'
+		item.name == name
+		item.serialNumber == sn
+		item.value == new BigDecimal(value)
+
+		and: 'the appropriate output is captured'
+		captureOutput.toString() == "Item name: Item serial number: Item value: "
+	}
+
+	private def simulateUserInput(String name, String sn, String value) {
+		new ByteArrayInputStream((name + System.lineSeparator() + sn + System.lineSeparator() + value).getBytes());
+	}
+
+}
